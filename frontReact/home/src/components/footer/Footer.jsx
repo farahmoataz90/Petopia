40791aa70b412d6064d0b9999cdc0bd0b1a03ef6
farah moataz <farahmoataz90@gmail.com>
2024-04-21 06:03:26 +0200

    zabat el footer , not fully completed

--- frontReact/home/src/components/footer/Footer.jsx
+++ frontReact/home/src/components/footer/Footer.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import './footer.css';
 import cat from './images/cta-banner.png';
-// import backG from './images/cta-bg.jpg';
+import backG from './images/cta-bg.jpg';
 
 function Footer() {
   return (
     <>
-    <section className="cta has-bg-image" aria-label="cta">
-        <div className="container">
+    <section className="CTA has-bg-image" aria-label="cta">
+        <div className="CONTAINER">
 
-          <figure className="cta-banner">
-            <img src={cat}  loading="lazy" alt="cat" className="w-100"/>
+          <figure className="CTA-BANNER">
+            <img src={cat} width="900" height="660" loading="lazy" alt="cat" className="w-100"/>
           </figure>
 
-          <div className="cta-content">
+          <div className="CTA-CONTENT">
 
-            <h2 class="h2 section-title">Taste it, love it or we’ll replace it… Guaranteed!</h2>
+            <h2 class="h2 SECTION-TITLE">Taste it, love it or we’ll replace it… Guaranteed!</h2>
 
-            <p className="section-text">
+            <p className="SECTION-TEXT">
               At Petio, we believe your dog and cat will love their food so much that if they don’t … we’ll help you
               find a
               replacement. That’s our taste guarantee.
             </p>
 
-            <a href="/" className="btn">Find out more</a>
+            <a href="/" className="BTN">Find out more</a>
 
           </div>
 
