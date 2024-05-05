5d1ba9b1b95ee5577743b8e3646fd5ff4c402164
Mazen Mostafa <106120157+MazenMostafa2025@users.noreply.github.com>
2024-05-05 16:12:18 +0300

    Add files via upload

---
+++ backNode/app.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const bodyParser = require('body-parser');
+
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./utils/globalErrorHandler');
+const userRouter = require('./Routes/userRoute');
+const productRouter = require('./Routes/productRoute');
+const orderRouter = require('./Routes/orderRoute');
+const cartRouter = require('./Routes/cartRoute');
+
+// Start express app
+const app = express();
+
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
+// if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+// }
+
+
+
+// Stripe webhook, BEFORE body-parser, because stripe needs the body as stream
+// app.post(
+//   '/webhook-checkout',
+//   bodyParser.raw({ type: 'application/json' }),
+//   bookingController.webhookCheckout
+// );
+
+// Body parser, reading data from body into req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// 3) ROUTES
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/order', orderRouter);
+app.use('/api/v1/cart', cartRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
+
+module.exports = app;
